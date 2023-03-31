f0e50b451615d138337a0cc6b1fa6d37a0b5de34
Miguel Quispe <dev@miguelquispe.com>
2023-03-31 05:23:36 -0500

    add song detail page

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,24 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
+import SongBar from './SongBar';
+
+const RelatedSongs = ({ data, isPlaying, activeSong, handlePauseClick, handlePlayClick, artistId }) => (
+  <div className="flex flex-col">
+    <h1 className="font-bold text-3xl text-white">Otras canciones:</h1>
+
+    <div className="mt-2">
+      {data?.map((song, i) => (
+        <SongBar
+          key={`${song.key}-${artistId}`}
+          song={song}
+          i={i}
+          artistId={artistId}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          handlePauseClick={handlePauseClick}
+          handlePlayClick={handlePlayClick}
+        />
+      ))}
+    </div>
+  </div>
 );
 
 export default RelatedSongs;
